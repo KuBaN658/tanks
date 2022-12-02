b4f60f45bb6f2346f137c69fbf6764515137bfa2
KuBaN658 <garkovski_dmitri@mail.ru>
2022-12-02 12:18:56 +0300

    движение пушки вверх-вниз

--- gun.py
+++ gun.py
@@ -94,6 +94,7 @@ class Gun:
         self.f2_on = 0
         self.an = 1
         self.color = GREY
+        self.pos = (1, 1)
 
     def fire2_start(self, event):
         self.f2_on = 1
@@ -118,14 +119,47 @@ class Gun:
     def targetting(self, event):
         """Прицеливание. Зависит от положения мыши."""
         if event:
-            self.an = math.atan((event.pos[1]-450) / (event.pos[0]-20))
+            self.an = math.atan((event.pos[1]-450) / (event.pos[0]-20.00001))
+            self.pos = event.pos
         if self.f2_on:
             self.color = RED
         else:
             self.color = GREY
 
     def draw(self):
-        pygame.draw.rect(self.screen, self.color, (0, 420, self.f2_power, 40))
+        width_gun = 10
+        distance_to_aim = math.hypot(20 - self.pos[0], 420 - self.pos[1])
+        scalar = distance_to_aim / self.f2_power
+        x1 = 20
+        y1 = 420
+        x2 = 20 + (self.pos[0] - 20) / scalar
+        y2 = 420 + (self.pos[1] - 420) / scalar
+        scalar = distance_to_aim / width_gun
+        x3 = x2 + (420 - self.pos[1]) / scalar
+        y3 = y2 + (self.pos[0] - 20) / scalar
+        x4 = x1 + (420 - self.pos[1]) / scalar
+        y4 = y1 + (self.pos[0] - 20) / scalar
+        pygame.draw.polygon(screen, self.color,  ((x1, y1), (x2, y2), (x3, y3), (x4, y4)))
+
+    # caunt_point_polygon(x0, y0, x1, y1, width):
+    # hypotenuse = ((x1 - x0) ** 2 + (y1 - y0) ** 2) ** 0.5
+    # x = abs(y1 - y0) / hypotenuse * width
+    # y = abs(x1 - x0) / hypotenuse * width
+    # x_point = x0 + x
+    # if y0 < y1:
+    #     y_point = y0 - y
+    # else:
+    #     y_point = y0 + y
+    # coords_second = x_point, y_point
+    # x_point = x1 + x
+    # if y0 < y1:
+    #     y_point = y1 - y
+    # else:
+    #     y_point = y1 + y
+    # coords_first = x_point, y_point
+    # coords = [coords_first, coords_second]
+    # coords = [(x0, y0), (x1, y1)] + coords
+    # return coords
 
     def power_up(self):
         if self.f2_on:
